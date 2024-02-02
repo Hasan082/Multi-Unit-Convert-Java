0bfbcedbf323984962f020132faded50c290fc40
Md Hasanuzzaman <dr.has82@gmail.com>
2024-02-03 05:13:07 +0600

    Temparature converter done

--- app/src/main/java/com/hasan/multiconvert/TemperatureActivity.java
+++ app/src/main/java/com/hasan/multiconvert/TemperatureActivity.java
@@ -4,14 +4,121 @@ import static com.hasan.multiconvert.utils.AppBarUtil.setAppBarTitle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hasan.multiconvert.utils.HideKeyBoard;
+
+import java.util.Locale;
 
 public class TemperatureActivity extends AppCompatActivity {
-
+    LinearLayout parentLayout;
+    String formattedValue;
+    Spinner spinner;
+    EditText editTextLengthValue;
+    Button btnConvert, btnClear;
+    Resources res;
+    String[] resourceUnits;
+    double inputValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
         setAppBarTitle(this);
+
+        parentLayout = findViewById(R.id.lengthParentView);
+        spinner = findViewById(R.id.spinnerUnits);
+        editTextLengthValue = findViewById(R.id.editTextLengthValue);
+        btnConvert = findViewById(R.id.btnConvert);
+        btnClear = findViewById(R.id.btnClear);
+        res = getResources();
+        resourceUnits = res.getStringArray(R.array.temperature_units);
+
+        btnConvert.setOnClickListener(v -> {
+            String edyTxtStr = editTextLengthValue.getText().toString().trim();
+
+            if(!edyTxtStr.isEmpty()){
+                inputValue = Double.parseDouble(edyTxtStr);
+                String spinnerText = spinner.getSelectedItem().toString();
+                double[] unitValArray = ConvertUnits(inputValue, spinnerText);
+
+                for (int i = 0; i < resourceUnits.length; i++) {
+                    View unitLayoutView = getLayoutInflater().inflate(R.layout.length_unit_layout, parentLayout, false);
+                    TextView unitName = unitLayoutView.findViewById(R.id.unitMetric);
+                    TextView unitVal = unitLayoutView.findViewById(R.id.unitValue);
+                    String kmValue = resourceUnits[i];
+                    String formattedText = kmValue + getString(R.string.unitIndicator) + " ";
+                    unitName.setText(formattedText);
+                    if (unitValArray[i] % 1 == 0) {
+                        formattedValue = String.format(Locale.US, "%.0f", unitValArray[i]);
+                    } else if (unitValArray[i] * 1000 % 1 == 0) {
+                        String stringValue = String.format(Locale.US, "%.3f", unitValArray[i]);
+                        formattedValue = stringValue.replaceAll("\\.?0*$", "");
+                    } else {
+                        String stringValue = String.format(Locale.US, "%.5f", unitValArray[i]);
+                        formattedValue = stringValue.replaceAll("\\.?0*$", "");
+                    }
+                    unitVal.setText(formattedValue);
+
+                    if (resourceUnits[i].equals(spinnerText)) {
+                        unitLayoutView.setVisibility(View.GONE);
+                    }
+
+                    parentLayout.addView(unitLayoutView);
+                }
+
+                editTextLengthValue.clearFocus();
+                HideKeyBoard.hideKeyboard(this, v);
+                btnConvert.setVisibility(View.GONE);
+                btnClear.setVisibility(View.VISIBLE);
+
+            } else {
+                HideKeyBoard.hideKeyboard(this, v);
+                Toast.makeText(this, "Please input length", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnClear.setOnClickListener(v -> {
+            editTextLengthValue.setText("");
+            spinner.setSelection(0);
+            parentLayout.removeAllViews();
+            btnConvert.setVisibility(View.VISIBLE);
+            btnClear.setVisibility(View.GONE);
+        });
+
+
     }
+
+    private double[] ConvertUnits(double inputValue, String spinnerText) {
+        double celsius, fahrenheit, kelvin;
+
+        if (spinnerText.contains("(°C)")) {
+            celsius = inputValue;
+            fahrenheit = (inputValue * 9/5) + 32;
+            kelvin = inputValue + 273.15;
+        } else if (spinnerText.contains("(°F)")) {
+            celsius = (inputValue - 32) * 5/9;
+            fahrenheit = inputValue;
+            kelvin = (inputValue - 32) * 5/9 + 273.15;
+        } else if (spinnerText.contains("(K)")) {
+            celsius = inputValue - 273.15;
+            fahrenheit = (inputValue - 273.15) * 9/5 + 32;
+            kelvin = inputValue;
+        } else {
+            celsius = fahrenheit = kelvin = 0.0;
+        }
+
+        return new double[]{celsius, fahrenheit, kelvin};
+    }
+
+
+
+
 }
